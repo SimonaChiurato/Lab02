602f472ae425ec3985ca9d9ab650f6e462bb1ec6
Simona <Simona@LAPTOP-2JANVKQT.homenet.telecomitalia.it>
2020-03-22 09:42:32 +0100

    creazione scene builder

--- Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
+++ Lab02_Alien/src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -1,11 +1,13 @@
+
 package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
@@ -14,27 +16,35 @@ public class FXMLController {
 
     @FXML
     private URL location;
-    
+
+    @FXML
+    private TextField txtParola;
+
     @FXML
     private Button btnTranslate;
-    
+
     @FXML
-    private Button btnReset;
+    private TextArea txtResult;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    void doClear(ActionEvent event) {
+
+    }
 
     @FXML
     void doTranslate(ActionEvent event) {
-    	// TODO - add the button and complete this    	
+
     }
-    
-    
-    @FXML
-    void doReset(ActionEvent event) {
-    	// TODO - add the button and complete this 
-    }
-    
-    
+
     @FXML
     void initialize() {
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnTranslate != null : "fx:id=\"btnTranslate\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
